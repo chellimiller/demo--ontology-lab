8a876ca8ff37290528d1656a6bdde7eb40a7848b
Michelle Miller <chellimiller@users.noreply.github.com>
2024-03-02 11:54:33 -0700

    Add ability to connect links

--- src/views/BuilderView/BuilderView.tsx
+++ src/views/BuilderView/BuilderView.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 /* eslint-disable class-methods-use-this */
 import * as React from 'react';
 import { uniqueId } from 'lodash';
@@ -17,6 +18,7 @@ type Node = {
 };
 
 type Link = {
+  id: string;
   source: string;
   target: string;
 };
@@ -44,43 +46,24 @@ class BuilderVizManager extends AbstractVizManager<unknown> {
     this.settings = Object.freeze({ height, width, nodeRadius });
     this.state = { nodes: new Map(), links: new Map() };
     this.addNode = this.addNode.bind(this);
+    this.addLink = this.addLink.bind(this);
+    this.isSelected = this.isSelected.bind(this);
+    this.handleNodeClick = this.handleNodeClick.bind(this);
     this.render = this.render.bind(this);
     this.setState = this.setState.bind(this);
     this.selection.attr('width', width).attr('height', height);
   }
 
-  render() {
-    // eslint-disable-next-line prefer-destructuring
-    const selection = this.selection;
+  addLink(link: SetRequired<Partial<Link>, 'source' | 'target'>) {
+    const { id = uniqueId('builder-link'), source, target } = link;
 
-    const links = selection
-      .selectAll('line')
-      .data(Array.from(this.state.links.values()))
-      .enter()
-      .append('line')
-      .style('stroke', '#AAA');
+    const links = new Map(this.state.links);
+    links.set(id, { id, source, target });
 
-    const nodes = selection
-      .selectAll('circle')
-      .data(Array.from(this.state.nodes.values()))
-      .enter()
-      .append('circle')
-      .attr('r', 20)
-      .attr('cx', (node) => node.x)
-      .attr('cy', (node) => node.y)
-      .style('fill', '#555')
-      .style('cursor', 'pointer')
-      .on('click', (event: PointerEvent) => {
-        console.log(event.target);
-        const element = event.target as SVGCircleElement;
-        selection.selectAll('circle').style('fill', '#555');
-        element.style.fill = '#999';
-      });
-  }
-
-  setState(state: BuilderState) {
-    this.state = state;
-    this.render();
+    this.setState({
+      ...this.state,
+      links,
+    });
   }
 
   addNode(node: SetRequired<Partial<Node>, 'name'>) {
@@ -102,6 +85,83 @@ class BuilderVizManager extends AbstractVizManager<unknown> {
       ...this.state,
       nodes,
     });
+  }
+
+  render() {
+    const { selection, state } = this;
+    const handleNodeClick = (event: PointerEvent) =>
+      this.handleNodeClick(event);
+    const selected = this.state.selected;
+    const nodes = Array.from(this.state.nodes.values());
+    const links = Array.from(this.state.links.values());
+
+    selection.selectAll('svg > *').remove();
+
+    selection
+      .selectAll('line')
+      .data(links)
+      .enter()
+      .append('line')
+      .attr('class', 'link')
+      .style('stroke', '#AAA')
+      .attr('x1', (link) => state.nodes.get(link.source)?.x ?? '0')
+      .attr('y1', (link) => state.nodes.get(link.source)?.y ?? '0')
+      .attr('x2', (link) => state.nodes.get(link.target)?.x ?? '0')
+      .attr('y2', (link) => state.nodes.get(link.target)?.y ?? '0');
+
+    selection
+      .selectAll('circle')
+      .data(nodes)
+      .enter()
+      .append('circle')
+      .attr('id', (node) => node.id)
+      .attr('class', (node) =>
+        node.id === selected ? 'node selected' : 'node'
+      )
+      .attr('r', this.settings.nodeRadius)
+      .attr('cx', (node) => node.x)
+      .attr('cy', (node) => node.y)
+      .style('fill', (node) => {
+        console.log('selected ', {
+          selected,
+          value: node.id === selected,
+          node: node.id,
+        });
+        return selected === node.id ? '#999' : '#555';
+      })
+      .style('cursor', 'pointer')
+      .on('click', handleNodeClick);
+  }
+
+  isSelected(id: string): boolean {
+    return this.state.selected === id;
+  }
+
+  handleNodeClick(event: PointerEvent) {
+    const element = event.target as SVGCircleElement;
+    const id = element.getAttribute('id');
+
+    if (!id) return;
+
+    if (this.isSelected(id)) {
+      this.setState({ ...this.state, selected: undefined });
+      return;
+    }
+
+    if (!event.ctrlKey) {
+      console.log(id);
+      this.setState({ ...this.state, selected: id });
+      return;
+    }
+
+    if (!this.state.selected) return;
+
+    this.addLink({ source: this.state.selected, target: id });
+  }
+
+  setState(state: BuilderState) {
+    this.state = state;
+    this.render();
   }
 }
 
